03ca7f38dddfb68803d3e8379ae49e8aa96ef0d0
i951 <44221438+i951@users.noreply.github.com>
2023-07-09 13:01:38 -0700

    Add tests for createUser

--- server/controllers/__tests__/user.test.js
+++ server/controllers/__tests__/user.test.js
@@ -1,19 +1,65 @@
-const app = require('../../index')
-const supertest = require('supertest')
+const app = require("../../index");
+const supertest = require("supertest");
 
-describe('createUser', () => {
-    test('invalid name', async () => {
-        const payload = {
-            userID: 'a',
-            name: '%%$#(@!',
-            email: 'a@b.c'
-        }
+describe("createUser", () => {
+  test("empty userID", async () => {
+    const payload = {
+      userID: "",
+      name: "%%$#(@!",
+      email: "a@b.c",
+    };
 
-        const res = await supertest(app)
-            .post('/users/')
-            .send(payload)
-        expect(res.status).toEqual(400)
-        expect(res.type).toEqual(expect.stringContaining('json'))
-        expect(res.body).toHaveProperty('error')
-    })
-})
+    const res = await supertest(app).post("/users/").send(payload);
+    expect(res.status).toEqual(400);
+    expect(res.type).toEqual(expect.stringContaining("json"));
+    expect(res.body).toHaveProperty("error");
+  });
+  test("empty name", async () => {
+    const payload = {
+      userID: "a",
+      name: "",
+      email: "a@b.c",
+    };
+
+    const res = await supertest(app).post("/users/").send(payload);
+    expect(res.status).toEqual(400);
+    expect(res.type).toEqual(expect.stringContaining("json"));
+    expect(res.body).toHaveProperty("error");
+  });
+  test("invalid name", async () => {
+    const payload = {
+      userID: "a",
+      name: "%%$#(@!",
+      email: "a@b.c",
+    };
+
+    const res = await supertest(app).post("/users/").send(payload);
+    expect(res.status).toEqual(400);
+    expect(res.type).toEqual(expect.stringContaining("json"));
+    expect(res.body).toHaveProperty("error");
+  });
+  test("empty email", async () => {
+    const payload = {
+      userID: "a",
+      name: "a",
+      email: "",
+    };
+
+    const res = await supertest(app).post("/users/").send(payload);
+    expect(res.status).toEqual(400);
+    expect(res.type).toEqual(expect.stringContaining("json"));
+    expect(res.body).toHaveProperty("error");
+  });
+  test("invalid email", async () => {
+    const payload = {
+      userID: "a",
+      name: "a",
+      email: "sdfsdf",
+    };
+
+    const res = await supertest(app).post("/users/").send(payload);
+    expect(res.status).toEqual(400);
+    expect(res.type).toEqual(expect.stringContaining("json"));
+    expect(res.body).toHaveProperty("error");
+  });
+});
