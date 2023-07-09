02a0665d309d028046faac50c613f5016c23c64e
i951 <44221438+i951@users.noreply.github.com>
2023-07-09 13:07:36 -0700

    Add validator

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,17 +1,21 @@
 const User = require("../models/user.model");
+var validator = require("validator");
 
 const userController = {
   createUser: (req, res) => {
-    const {
-      userID,
-      name, 
-      email
-    } = req.body;
+    const { userID, name, email } = req.body;
+
+    if (!validator.isAlpha(name)) {
+      return res.status(400).json({ error: "Name must contain letters only" });
+    }
+    if (!validator.isEmail(email)) {
+      return res.status(400).json({ error: "Invalid email" });
+    }
 
     let newUser = User({
       __id: userID,
-      name, 
-      email
+      name,
+      email,
     });
 
     newUser.save((err) => {
