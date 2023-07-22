a0070667ef197d6524ce84fc103e4b3391aa2666
i951 <44221438+i951@users.noreply.github.com>
2023-07-21 23:37:34 -0700

    Refactor Mongoose connection to remove callbacks

--- server/index.js
+++ server/index.js
@@ -10,17 +10,6 @@ const postRouter = require("./routes/post.route.js");
 // MongoDB
 const uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.qafmunu.mongodb.net/?retryWrites=true&w=majority`;
 mongoose.set("strictQuery", false);
-try {
-  mongoose.connect(
-    uri,
-    { useNewUrlParser: true, useUnifiedTopology: true },
-    () => {
-      console.log("MongoDB connected");
-    }
-  );
-} catch (error) {
-  console.log("Could not connect to MongoDB");
-}
 
 const app = express();
 
@@ -33,14 +22,28 @@ app.use("/posts", postRouter);
 const port = 5205;
 const server = http.createServer(app);
 
-// Start server
-server.listen(port);
+// Connect to database and start server
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(
+    () => {
+      console.log("MongoDB connected");
+      server.listen(port);
+    },
+    (err) => {
+      console.log("Could not connect to MongoDB");
+    }
+  );
+
 server.on("listening", () => {
-  console.log("Listening on port", port);
+  console.log("Server listening on port", port);
 });
 
 app.get("/", (req, res) => {
   res.send("ʕ ·(エ)· ʔ");
 });
 
-module.exports = app
+module.exports = app;
