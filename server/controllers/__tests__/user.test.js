27a6aa77ad0c51dee6ec4517b41203aa52f13812
i951 <44221438+i951@users.noreply.github.com>
2023-07-21 23:06:34 -0700

    Update tests to check response body

--- server/controllers/__tests__/user.test.js
+++ server/controllers/__tests__/user.test.js
@@ -5,43 +5,49 @@ describe("createUser", () => {
   test("empty userID", async () => {
     const payload = {
       userID: "",
-      name: "%%$#(@!",
-      email: "a@b.c",
+      name: "abc",
+      email: "a@b.com",
     };
 
     const res = await supertest(app).post("/users/").send(payload);
     expect(res.status).toEqual(422);
     expect(res.type).toEqual(expect.stringContaining("json"));
     expect(res.body).toHaveProperty("errors");
+    expect(res.body.errors).toHaveLength(1);
+    expect(res.body.errors[0].path).toEqual("userID");
   });
   test("empty name", async () => {
     const payload = {
-      userID: "a",
+      userID: "aaaaa",
       name: "",
-      email: "a@b.c",
+      email: "a@b.com",
     };
 
     const res = await supertest(app).post("/users/").send(payload);
     expect(res.status).toEqual(422);
     expect(res.type).toEqual(expect.stringContaining("json"));
     expect(res.body).toHaveProperty("errors");
+    expect(res.body.errors).toHaveLength(1);
+    expect(res.body.errors[0].path).toEqual("name");
   });
   test("invalid name", async () => {
     const payload = {
-      userID: "a",
+      userID: "aaaaa",
       name: "%%$#(@!",
-      email: "a@b.c",
+      email: "a@b.com",
     };
 
     const res = await supertest(app).post("/users/").send(payload);
     expect(res.status).toEqual(422);
     expect(res.type).toEqual(expect.stringContaining("json"));
     expect(res.body).toHaveProperty("errors");
+    expect(res.body.errors).toHaveLength(1);
+    expect(res.body.errors[0].path).toEqual("name");
   });
   test("empty email", async () => {
     const payload = {
-      userID: "a",
-      name: "a",
+      userID: "aaaaa",
+      name: "aaaaa",
       email: "",
     };
 
@@ -49,11 +55,13 @@ describe("createUser", () => {
     expect(res.status).toEqual(422);
     expect(res.type).toEqual(expect.stringContaining("json"));
     expect(res.body).toHaveProperty("errors");
+    expect(res.body.errors).toHaveLength(1);
+    expect(res.body.errors[0].path).toEqual("email");
   });
   test("invalid email", async () => {
     const payload = {
-      userID: "a",
-      name: "a",
+      userID: "aaaaa",
+      name: "aaaaa",
       email: "sdfsdf",
     };
 
@@ -61,5 +69,7 @@ describe("createUser", () => {
     expect(res.status).toEqual(422);
     expect(res.type).toEqual(expect.stringContaining("json"));
     expect(res.body).toHaveProperty("errors");
+    expect(res.body.errors).toHaveLength(1);
+    expect(res.body.errors[0].path).toEqual("email");
   });
 });
