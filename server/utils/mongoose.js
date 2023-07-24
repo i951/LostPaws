05f19e948683939c1ee34afd1c53cd6cd1365544
i951 <44221438+i951@users.noreply.github.com>
2023-07-23 17:56:57 -0700

    Change get env vars for mongodb

--- server/utils/mongoose.js
+++ server/utils/mongoose.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
-const uri = process.env.MONGODB_URI;
+// const uri = process.env.MONGODB_URI;
+const uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.qafmunu.mongodb.net/?retryWrites=true&w=majority`;
+mongoose.set("strictQuery", false);
 
 function dbconnect() {
   mongoose.connect(uri, {
